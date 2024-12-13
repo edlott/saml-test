c8cb8100cdf88201e99998b41362bb4ffc5b5c3f
Ed Lott <edlott@sbcglobal.net>
2024-12-13 09:39:30 -0500

    Fixes

--- src/Logout.js
+++ src/Logout.js
@@ -2,13 +2,15 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 const Logout = () => {
     return (
-       <h1>
-           Logged Out
-       </h1>
-       <ul>
-          <li><a href="http://localhost:4000">IDP Launch Page</a></li>
-          <li><Link to="/">Login</Link></li>
-       </ul>
+       <div>
+           <h1>
+               Logged Out
+           </h1>
+           <ul>
+              <li><a href="http://localhost:4000">IDP Launch Page</a></li>
+              <li><Link to="/">Login</Link></li>
+           </ul>
+       </div>
     );
 }
 
